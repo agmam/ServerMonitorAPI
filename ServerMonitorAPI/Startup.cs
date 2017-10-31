2372e6326c7e4fccf535f327e10fe359f821cdea
agmam <ag-mam@live.dk>
2017-10-31 13:42:47 +0100

    security

--- ServerMonitorAPI/Startup.cs
+++ ServerMonitorAPI/Startup.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Owin;
 using Owin;
+using ServerMonitorAPI.Models;
 
 [assembly: OwinStartup(typeof(ServerMonitorAPI.Startup))]
 
@@ -12,6 +15,16 @@ namespace ServerMonitorAPI
     {
         public void Configuration(IAppBuilder app)
         {
+            ApplicationDbContext context = new ApplicationDbContext();
+            var userManager = new ApplicationUserManager(new UserStore<ApplicationUser>(context));
+            var name = "server@monitor.dk";
+            var user = new ApplicationUser();
+            user.UserName = name;
+            user.Email = name;
+            string userPWD = "server123";
+
+            var result = userManager.Create(user, userPWD);
+
             ConfigureAuth(app);
         }
     }
